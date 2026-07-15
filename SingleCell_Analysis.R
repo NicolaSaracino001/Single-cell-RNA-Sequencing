25de47d03cd2e232ac9b62b37510ae0526935461
Nicola Saracino <nicola.saracino098@gmail.com>
2026-07-15 14:47:18 +0200

    Aggiunta dei commenti

--- SingleCell_Analysis.R
+++ SingleCell_Analysis.R
@@ -1,8 +1,18 @@
 setwd("/Volumes/Archivio_MacNicola/Progetti UNIVERSITA/Transcriptomics_project_SC")
 getwd
+
+# Data Loading & Setup
+
 library(dplyr)
+
 #install.packages("Seurat")
 library(Seurat)
 library(patchwork)
 library(ggplot2)
+
+# Load PanglaoDB data
 load("SRA701877_SRS3279692.sparse.RData")
+
+# Mini-Challenge: Clean up gene names
+# Remove the ENSEMBL ID (everything after the underscore)
+
