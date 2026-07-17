58e939063c1a6fc844994ef97e32a7014fe5adca
Nicola Saracino <nicola.saracino098@gmail.com>
2026-07-17 15:11:35 +0200

    Completata la data loading e setup

--- SingleCell_Analysis.R
+++ SingleCell_Analysis.R
@@ -15,4 +15,22 @@ load("SRA701877_SRS3279692.sparse.RData")
 
 # Mini-Challenge: Clean up gene names
 # Remove the ENSEMBL ID (everything after the underscore)
+sm@Dimnames[[1]] <- sub("[_].*","", sm@Dimnames[[1]])
+head(sm)
 
+# Handle duplicate gene names
+# Check if there are duplicates (if it returns a number > 0, there are duplicate)
+anyDuplicated(rownames(sm))
+
+# Make gene names unique, as required by the professor 
+rownames(sm) <- make.unique(rownames(sm))
+
+# Initialize the Seurat object 
+# We name our object "PanIslets_Nicola"
+PanIslets_Nicola <- CreateSeuratObject(counts = sm,
+                                project = "PanIslets", min.cells = 3,
+                                min.features = 200)
+
+# Take a look at the newly created abject to see how many cells and genes we have 
+PanIslets_Nicola
+head(PanIslets_Nicola)
